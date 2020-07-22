52818547aeb97dcbf29b17e6981c244186569015
tkl1130 <60303910+tkl1130@users.noreply.github.com>
2020-07-21 21:39:12 -0400

    removed weather api info and updated to retrieve file name

--- CovidDataRetriever.py
+++ CovidDataRetriever.py
@@ -1,30 +1,23 @@
 import json
 import sys
 import requests
+# simple script to call the State of Indiana API to retreive information
+#script is to play around and test calling of API through Python
 Baseurl ='https://hub.mph.in.gov/api/3/action/'
 #display relevant info
-ListPackages='package_list'
-response = requests.get(Baseurl+ListPackages)
-response.raise_for_status()
-ListGroups='group_list'
-response = requests.get(Baseurl+ListGroups)
-response.raise_for_status()
+#ListPackages='package_list'
+#response = requests.get(Baseurl+ListPackages)
+#response.raise_for_status()
+#ListGroups='group_list'
+#response = requests.get(Baseurl+ListGroups)
+#response.raise_for_status()
 # Load JSON data into a Python variable.
-RetrieveData = json.loads(response.text)
-print(RetrieveData)
-#print covid case data
+#RetrieveData = json.loads(response.text)
 ListCovidCaseData='package_show?id=covid-19-case-data'
 response = requests.get(Baseurl+ListCovidCaseData)
 response.raise_for_status()
 RetrieveData = json.loads(response.text)
-print(RetrieveData)
-#print details of covid packages
-#print(" ")
-#w = weatherData['weather']
-#print(w)
-#print (weatherDict)
-# Print weather descriptions.
-#w = weatherData['weather']
-#print (w)
-#print('Current weather in %s:' % (location))
-#print([w]['main'], '-', w[0]['weather'][0]['description'])
+resourcesURL = RetrieveData["result"]["resources"]
+FileURL = resourcesURL[0]["url"]
+print(FileURL)
+
